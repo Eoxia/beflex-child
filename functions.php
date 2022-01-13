0371c281e6e19da40e6215942f9c51d97ff66cb0
eoxia-amandine <amandine@eoxia.com>
2022-01-13 17:58:54 +0100

    fix function

--- functions.php
+++ functions.php
@@ -31,7 +31,7 @@ function theme_child_enqueue_style() {
 	wp_enqueue_style( 'custom-child-style', get_stylesheet_directory_uri() . '/style.css' );
 
 	/** Scripts */
-	wp_enqueue_script( 'beflex-child-js', get_stylesheet_directory_uri() . 'assets/js/theme.js' );
+	wp_enqueue_script( 'beflex-child-js', get_stylesheet_directory_uri() . '/assets/js/theme.js' );
 }
 add_action( 'wp_enqueue_scripts', 'theme_child_enqueue_style', 11 );
 
