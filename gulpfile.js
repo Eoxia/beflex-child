11caafdf292a003df00397af4a72c8c419755a2c
eoxia-amandine <amandine@eoxia.com>
2022-12-14 17:17:44 +0100

    Refactor of Gulpfile SCSS

--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,41 @@
 
 var gulp         = require('gulp');
 var watch        = require('gulp-watch');
-var concat       = require('gulp-concat');
 var sass         = require('gulp-sass')(require('sass'));
 var rename       = require('gulp-rename');
-var uglify       = require('gulp-uglify');
 var autoprefixer = require('gulp-autoprefixer');
+var lec          = require('gulp-line-ending-corrector');
+var path         = require('path');
 
-var paths = {
-	scss: [ 'assets/scss/**/*.scss', 'assets/css/' ],
-};
+var scss_paths = [
+	'assets/scss/**/*.scss',
+	// 'inc/blocks/test/assets/scss/**/*.scss'
+];
 
 /** SCSS */
 gulp.task( 'build_scss', function() {
-	return gulp.src( paths.scss[0] )
+	return gulp.src( scss_paths )
 		.pipe( sass( { 'outputStyle': 'expanded' } ).on( 'error', sass.logError ) )
 		.pipe( autoprefixer({
 			browsers: ['last 2 versions'],
 			cascade: false
 		}) )
-		.pipe( gulp.dest( paths.scss[1] ) )
+		.pipe(lec({verbose:true, eolc: 'CRLF', encoding:'utf8'}))
+		.pipe( gulp.dest(function(file) {
+			return path.join(file.base, '..', 'css' );
+		}) )
 		.pipe( sass({outputStyle: 'compressed'}).on( 'error', sass.logError ) )
-		.pipe( rename( './style.min.css' ) )
-		.pipe( gulp.dest( paths.scss[1] ) );
+		.pipe(rename(function (path) {
+			path.basename += ".min";
+			path.extname = ".css";
+		}))
+		.pipe(lec({verbose:true, eolc: 'CRLF', encoding:'utf8'}))
+		.pipe( gulp.dest(function(file) {
+			return path.join(file.base, '..', 'css' );
+		}) )
 });
 
 /** Watch */
 gulp.task( 'default', function() {
-	gulp.watch( paths.scss[0], gulp.series('build_scss') );
-	gulp.watch( paths.js[0], gulp.series('build_js') );
+	gulp.watch( scss_paths, gulp.series('build_scss') );
 });
